9ce36b236573bae7bcc53bfb419c351b3a5fd838
qiaoq <qiaoq@EVEPC>
2024-04-22 16:43:31 +0800

    scene1 move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f; // ??????
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // ??????????
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // ???????
+        Vector2 movement = new Vector2(horizontalInput, 0f) * moveSpeed;
+
+        // ?? Animator ??????
+        animator.SetFloat("Speed", Mathf.Abs(movement.x));
+
+        // ??????
+        rb.velocity = new Vector2(movement.x, rb.velocity.y);
+
+        // ??????
+        if (horizontalInput > 0f)
+        {
+            spriteRenderer.flipX = false; // ???????
+        }
+        else if (horizontalInput < 0f)
+        {
+            spriteRenderer.flipX = true; // ??????
+        }
     }
 }
