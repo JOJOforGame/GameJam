b4bf1ae53cd2acc912312724f0f887f6aa65b99f
zzz <zzz@zzz.zzz>
2024-04-24 20:06:20 +0800

    Add dialog

---
+++ Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    private bool playerInRange;
+    public Dialog Dialog;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player in range");
+            playerInRange = true;
+            Dialog.DialogIndicator.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player out of range");
+            playerInRange = false;
+            Dialog.DialogIndicator.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange = true && Input.GetKeyDown(KeyCode.E))
+        {
+            Dialog.StartDialog();
+        }
+    }
+}
